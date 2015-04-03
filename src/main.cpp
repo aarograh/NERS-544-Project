21fd230cb5318ab4f39e702d243c4d031df8226e
Aaron Graham <aarograh@umich.edu>
2015-04-03 11:37:51 -0400

    Adding main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+// AUTHORS: Aaron Graham, Mike Jarrett
+// PURPOSE: NERS 544 Course Project
+// DATE   : April 3, 2015
+
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}
