252b4c79a3632cfe454c0a483da05651e3ab0a5e
Aaron Graham <aarograh@umich.edu>
2015-04-16 16:57:44 -0400

    Additions to geometry.h for some initialization stuff.

--- src/geometry.h
+++ src/geometry.h
@@ -2,17 +2,32 @@
 // PURPOSE: NERS 544 Course Project
 // DATE   : April 3, 2015
 
-//TODO Extend this type for planes, cylinders, or whatever else we need
+#include<vector>
+
 class surface{
   public:
     int id;
+    surface();
+};
+
+class plane : surface{
+  public:
+    plane();
+};
+
+class cylinder : surface{
+  public:
+    cylinder();
 };
 
 class cell{
   public:
     int id; // cell id number
-    int *iSurfs; // id numbers of surfaces which create this cell
-    int *surfSens; // senses for each surface in iSurfs. 1 is +, 0 is -
+    std::vector<int> iSurfs; // id numbers of surfaces which create this cell
+    std::vector<int> surfSens; // senses for each surface in iSurfs. 1 is +, 0 is -
     int matid;
+    cell();
     double distToSurf(double, double, double);
 };
+
+void initPinCell(double);
