23158937a465b1906713d8cd3acfe09a4f6f4a39
Aaron Graham <aarograh@umich.edu>
2015-04-17 15:34:09 -0400

    Many updates to geometry.cpp
    1.  Filled in constructor functions for planes, cylinders, and cells
    2.  Added logic to initPinCell to create cells and surfaces reuqired
        for a pin cell.
    
    We still need to assign a material ID for the cells for when the
    neutrons switch regions, and distances to surfaces need to be
    calculated.

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -7,25 +7,49 @@
 std::vector<surface> surfaceList;
 std::vector<cell> cellList;
 
+const int xplane = 1, yplane = 2, zplane = 3;
+
 surface::surface()
 {
   id = 0;
 }
 
-plane::plane()
+plane::plane(int surfid, double position, int orientation)
 {
-  id = 0;
+  id = surfid;
+  switch(orientation)
+  {
+    case xplane:
+      point[0] = position; point[1] = 0.0; point[2] = 0.0;
+      norm[0] = 1.0; norm[1] = 0.0; norm[2] = 0.0;
+      break;
+    case yplane:
+      point[0] = 0.0; point[1] = position; point[2] = 0.0;
+      norm[0] = 0.0; norm[1] = 1.0; norm[2] = 0.0;
+      break;
+    case zplane:
+      point[0] = 0.0; point[1] = 0.0; point[2] = position;
+      norm[0] = 0.0; norm[1] = 0.0; norm[2] = 1.0;
+      break;
+  }
 }
 
-cylinder::cylinder()
+cylinder::cylinder(int surfid, double x, double y, double z, double R)
 {
-  id = 0;
+  id = surfid;
+  origin[0] = x; origin[1] = y; origin[2] = z;
+  radius = R;
 }
 
-cell::cell()
+cell::cell(int cellid, int size, int* surfs, int* sense)
 {
-  id = 0;
+  id = cellid;
   matid = 0;
+  for (int i = 0; i < size; i++, surfs++, sense++)
+  {
+    iSurfs.push_back(*surfs);
+    senses.push_back(*sense);
+  }
 }
 
 double cell::distToSurf(double x, double y, double z)
@@ -39,8 +63,35 @@ void initPinCell(double pitch)
   double height = 100.0;
   double radius = 1.5;
 
+// Build the fuel pin
+  // Construct cylinder for fuel pin
+  surfaceList.push_back(cylinder(surfaceList.size()+1,0.0,0.0,0.0,1.5));
+  // Construct top plane
+  surfaceList.push_back(plane(surfaceList.size()+1,100.0,zplane));
+  // Construct bottom plane
+  surfaceList.push_back(plane(surfaceList.size()+1,0.0,zplane));
+  // Construct the cell
+  {
+    int isurfs[3] = {0,1,2};
+    int sense[3] = {-1,-1,1};
+    cellList.push_back(cell(cellList.size()+1,3,isurfs,sense));
+  }
+
+// Construct the "box" for the moderator
   // Construct left plane
-  surfaceList.push_back(surface());
+  surfaceList.push_back(plane(surfaceList.size()+1,-halfpitch,xplane));
+  // Construct right plane
+  surfaceList.push_back(plane(surfaceList.size()+1,halfpitch,xplane));
+  // Construct front plane
+  surfaceList.push_back(plane(surfaceList.size()+1,-halfpitch,yplane));
+  // Construct back plane
+  surfaceList.push_back(plane(surfaceList.size()+1,halfpitch,yplane));
+  // Construct the cell
+  {
+    int isurfs[7] = {0,1,2,3,4,5,6};
+    int sense[7] = {1,-1,1,1,-1,1,-1};
+    cellList.push_back(cell(cellList.size()+1,7,isurfs,sense));
+  }
 
   return;
 }
