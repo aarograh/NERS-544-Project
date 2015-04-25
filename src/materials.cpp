beaca3d42021b626d18e714447e68bf7a22b8d38
Aaron Graham <aarograh@umich.edu>
2015-04-24 23:13:21 -0400

    Fixing some includes in materials.cpp

--- src/materials.cpp
+++ src/materials.cpp
@@ -2,11 +2,8 @@
 // PURPOSE: NERS 544 Course Project
 // DATE   : April 3, 2015
 
-#include<cstdlib>
-#include<cmath>
-#include<vector>
-#include "materials.h"
 #include "utils.h"
+#include "materials.h"
 
 std::vector<material*> materialList;
 
